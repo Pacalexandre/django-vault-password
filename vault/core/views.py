a89f907db2ac0c655cae3047f569b24717fcd8b7
Paulo Alexandre <pacalexandre@gmail.com>
2022-08-13 11:03:58 +0000

    correcoes

--- vault/core/views.py
+++ vault/core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html',context={'dados': 'new date'})
+    coins = [{'name':'batman','price':1.23 , 'symbol':'aa' , 'rank':1},
+    {'name':'robin','price':1.23 , 'symbol':'aa' , 'rank':2},
+    {'name':'gata','price':1.23 , 'symbol':'aa' , 'rank':3},
+    ]
+    return render(request, 'index.html',context={'coins': coins})
---
+++ vault/static/prod.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue;
+const App = {
+    data() {
+        return {
+            coin: '',
+            coins: null
+        }
+    },
+    created() {
+        this.coins = {{ coins }}
+        // const socket = new WebSocket(`ws://${window.location.host}/ws/crypto/`);
+        // let _this = this;
+        // socket.onmessage = function (event) {
+        //     _this.coins = JSON.parse(event.data);
+        //     console.log(_this.coins)
+        // }
+    }
+
+}
+createApp(App).mount('#app')
