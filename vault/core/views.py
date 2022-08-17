f87a2709f3a9866cacac87c90123e6423ad443f0
Paulo Alexandre <pacalexandre@gmail.com>
2022-08-17 00:07:02 +0000

    carraga dados para vue

--- vault/core/views.py
+++ vault/core/views.py
@@ -1,8 +1,11 @@
+import json
+
+import json
 from django.shortcuts import render
 
 def index(request):
-    coins = [{'name':'batman','price':1.23 , 'symbol':'aa' , 'rank':1},
+    dados = [{'name':'batman','price':1.23 , 'symbol':'aa' , 'rank':1},
     {'name':'robin','price':1.23 , 'symbol':'aa' , 'rank':2},
     {'name':'gata','price':1.23 , 'symbol':'aa' , 'rank':3},
     ]
-    return render(request, 'index.html',context={'coins': coins})
+    return render(request, 'index.html',context={'dados': json.dumps(dados)})
