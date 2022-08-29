eebc6942beecccdf78d940a0d6065a782d3fbd33
Paulo Alexandre <pacalexandre@gmail.com>
2022-08-29 01:17:24 +0000

    atualizacao

--- vault/core/views.py
+++ vault/core/views.py
@@ -7,3 +7,6 @@ def index(request):
     {'name':'gata','price':1.23 , 'symbol':'aa' , 'rank':3},
     ]
     return render(request, 'index.html',context={'data_django': json.dumps(dados)})
+
+def login(request):
+    return render(request, 'login.html', context={'username':None,'password':None})
